f4839106b8400c8b27c4d31e4319909024ef6458
Danijel Chalupka <danijel@simple-task.com>
2019-04-01 12:42:40 +0200

    navigation

--- www/front_src/src/redux/reducers/navigationReducer.js
+++ www/front_src/src/redux/reducers/navigationReducer.js
@@ -2,18 +2,11 @@ import * as actions from "../actions/navigationActions";
 
 // by default, no one menu entry is allowed
 const initialState = {
-  entries: [],
-  menuItems: {}
+  entries: []
 };
 
 const navigationReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actions.FETCH_NAVIGATION_SUCCESS:
-      return {
-        ...state,
-        entries: action.entries,
-        menuItems: action.menuItems
-      }
     case actions.SET_NAVIGATION_DATA:
       return {
         ...state,
